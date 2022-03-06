2565d3def3027747593ea85a8c0bc5186bac8c3d
faisal isfahani <workwfaisal@gmail.com>
2022-03-06 17:59:58 +0700

    hero improvement, add lottie and update loading container

--- src/components/LoadingIndicator/index.jsx
+++ src/components/LoadingIndicator/index.jsx
@@ -6,6 +6,9 @@ const LoadingIndicator = () => {
   const SpinnerWrapper = styled.div`
     height: 100%;
     width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   `;
   return (
     <SpinnerWrapper>
--- src/containers/Home/Hero.jsx
+++ src/containers/Home/Hero.jsx
@@ -1,20 +1,61 @@
 import Container from '@/components/Container';
-import { Spinner, Text } from '@chakra-ui/react';
-import React, { useState } from 'react';
+import { Box, Text } from '@chakra-ui/react';
+import React from 'react';
 import styled from '@emotion/styled';
 import InteractiveIframe from '@/components/InteractiveIframe/Loadable';
+import Lottie from 'react-lottie';
+import animationData from './underline';
 
 const TextHero = styled(Text)`
   font-size: 72px;
-  width: 55vw !important;
   font-weight: 900;
   line-height: 110%;
 `;
 
+const StyledAnchor = styled.a`
+  color: #0e6ffe;
+  text-decoration: none;
+  cursor: pointer;
+  :hover {
+    text-decoration: underline;
+  }
+`;
+
+const WithUnderLine = styled.span`
+  position: relative;
+`;
+
+const ImageContainer = styled.span`
+  position: absolute;
+  left: 0;
+  bottom: -45px;
+`;
 const Hero = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
   return (
     <Container display='flex' height='80vh' alignItems='center' fluid pl='9rem'>
-      <TextHero>We are passionate about deliver beautiful things.</TextHero>
+      <Box display='flex' flexDirection='column' rowGap='2rem' width='55vw'>
+        <TextHero>
+          We are passionate about deliver{' '}
+          <WithUnderLine>
+            beautiful{' '}
+            <ImageContainer>
+              <Lottie options={defaultOptions} width={300} />
+            </ImageContainer>
+          </WithUnderLine>{' '}
+          things.
+        </TextHero>
+        <StyledAnchor to='mailto:faisal.isfahani@gmail.com'>
+          Have a project in mind? Let&apos;s get to work.
+        </StyledAnchor>
+      </Box>
       <InteractiveIframe />
     </Container>
   );
